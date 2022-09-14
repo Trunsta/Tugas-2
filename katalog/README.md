1c4805bfa5c5d7a5c0522a1b76df0478661a131f
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-09-14 11:52:10 +0700

    first commit

---
+++ katalog/README.md
@@ -0,0 +1,2 @@
+Jelaskan bagaimana cara kamu mengimplementasikan poin 1 sampai dengan 4 di atas.
+1. 
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+import imp
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'katalog_item' : data_catalog_item,
+        'nama' : 'Nikolas Reva Bellie',
+        'NPM' : '2106750212'
+    }
+    return render(request, "katalog.html", context)
