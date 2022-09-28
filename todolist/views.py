e6329baf839cf7127f2397b2c09cfd71709e6ec8
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-09-28 14:47:57 +0700

    First commit T4

---
+++ todolist/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from todolist.models import Task
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from todolist.forms import TaskForm
+
+# Create your views here.
+@login_required(login_url='/todolist/login/')
+def show_todolist(request):
+    task_data = Task.objects.filter(user = request.user)
+    context = {
+        'data' : task_data,
+        'nama' : 'Nikolas Reva Bellie',
+        'npm' : '2106750212'
+    }
+    return render(request, "todolist.html", context)
+
+@login_required(login_url='/todolist/login/')
+def create_task(request):
+    if(request.method == "POST"):
+        form = TaskForm(request.POST)
+        if(form.is_valid):
+            form.instance.user = request.user
+            form.save()
+           
+        return redirect('todolist:show_todolist')
+    context = {}
+    return render(request, 'create_task.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('todolist:show_todolist')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('todolist:login')
