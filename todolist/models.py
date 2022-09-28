662f9e5c8d7f5d06392da130f41cbf8c7b34b74d
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-09-28 14:57:35 +0700

    Fix date field

--- todolist/models.py
+++ todolist/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(default = datetime.now())
     title = models.TextField()
     description = models.TextField()
